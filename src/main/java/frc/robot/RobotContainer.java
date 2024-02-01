21814951b4bad69edc2c5bb16a6311b64616a9b8
BillDunlap <williamwdunlap@gmail.com>
2024-02-01 15:46:47 -0800

    Test of SparkMax "smart motion".  Tuned for unloaded NEO.

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,79 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.commands.BrushlessGoToPositionSmartly;
+import frc.robot.commands.TestBrushlessDefaultCommand;
+// import frc.robot.commands.BrushlessSetPercentSpeed; // could use this in teleop
+import frc.robot.subsystems.SparkMaxBrushless;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+//import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final SparkMaxBrushless m_testBrushless = new SparkMaxBrushless(Constants.TestBrushless.canId, Constants.TestBrushless.encoderRotationsPerFinalRotation, Constants.TestBrushless.name);
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final XboxController m_driverController =
+      new XboxController(Constants.Operator.kDriverControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    m_testBrushless.setPIDCoefficients(Constants.TestBrushless.kP,
+                                       Constants.TestBrushless.kI, 
+                                       Constants.TestBrushless.kD,
+                                       Constants.TestBrushless.kIZone, 
+                                       Constants.TestBrushless.kFeedForward,
+                                       Constants.TestBrushless.kMinOutput,
+                                       Constants.TestBrushless.kMaxOutput);
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    new JoystickButton(m_driverController, XboxController.Button.kX.value)
+      .onTrue(new BrushlessGoToPositionSmartly(m_testBrushless, 0.0, 500.0, 0.0, 1250.0, 0.0));
+    new JoystickButton(m_driverController, XboxController.Button.kB.value)
+      .onTrue(new BrushlessGoToPositionSmartly(m_testBrushless, 100.0, 500.0, 0.0, 1250.0, 0.0));
+    new JoystickButton(m_driverController, XboxController.Button.kA.value)
+      .onTrue(new BrushlessGoToPositionSmartly(m_testBrushless, -50., 500.0, 0, 1250.0, 0.0));
+    new JoystickButton(m_driverController, XboxController.Button.kStart.value)
+      .onTrue(new InstantCommand(() -> m_testBrushless.setCurrentPositionAsInitialEncoderPosition()));
+    new Trigger(() -> Math.abs(m_driverController.getLeftY()) > 0.1)
+      .whileTrue(new TestBrushlessDefaultCommand(m_testBrushless, m_driverController));
+    //new JoystickButton(m_driverController, XboxController.Button.kY.value) // will this hold current position? seems to go to 0
+    //  .onTrue(new BrushlessGoToPositionSmartly(m_testBrushless, m_testBrushless.getPosition(), 500., 0, 2500., 0.0));
+    }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    // return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
+  }
+}
---
+++ src/main/java/frc/robot/commands/BrushlessGoToPositionSmartly.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SparkMaxBrushless;
+
+public class BrushlessGoToPositionSmartly extends Command {
+  private final SparkMaxBrushless m_motor;
+  private final double m_desiredPosition;
+  // velocity PID coefficients should have already been set in the motor object
+  // smart motion coefficients
+  private final double m_maxVelocity, m_minVelocity, m_maxAcceleration, m_allowedClosedLoopError;
+
+
+  /** Creates a new BrushlessGoToPositionSmartly. */
+  public BrushlessGoToPositionSmartly(SparkMaxBrushless motor, double desiredPosition,
+    double maxVelocity, double minVelocity, double maxAcceleration, double allowedClosedLoopError) {
+    m_motor = motor;
+    m_desiredPosition = desiredPosition;
+    m_maxVelocity = maxVelocity; m_minVelocity = minVelocity; m_maxAcceleration = maxAcceleration; m_allowedClosedLoopError = allowedClosedLoopError; 
+    addRequirements(m_motor);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    // we could reset to factory defaults and set pid coefs here
+    m_motor.setSmartMotionCoefficients(m_desiredPosition, m_maxVelocity, m_minVelocity, m_maxAcceleration, m_allowedClosedLoopError);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
